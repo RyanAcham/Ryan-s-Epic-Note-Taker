82ab409f6dc98e2164132ee27a37cd1921588f89
Ryan Acham <ryanacham@hotmail.com>
2022-06-17 07:19:49 -0400

    routing

--- server.js
+++ server.js
@@ -5,5 +5,22 @@ const path = require("path");
 const app = express();
 //listens to see if heroku can find a port; default port is 8000
 const PORT = process.env.PORT || 8000;
+//setting up express directories
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+//routing
+//notes routing
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//routing for saved notes
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+//routing to index
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 app.listen(PORT, () => console.log("Listening on port "+ PORT));
